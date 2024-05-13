ecc7d31002b17242105bb7367aece4f2ef3ae1d0
tniromin <tniromin2@gmail.com>
2024-05-13 19:24:20 +0530

    First draw

---
+++ Warmup Challenges/salesbymatch.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    s=set(ar)   
+    c=0
+    for i in s:
+        c+=ar.count(i)//2
+    #sum([ar.count(i)//2 for i in s ])
+    return c
+             
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
